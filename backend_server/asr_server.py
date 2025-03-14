f5c7ec0c3336bd822408f8a31b93069d1168c1e7
pranavagrawaI <hipranav22@gmail.com>
2025-03-14 16:57:33 +0530

    update server slightly

--- backend_server/asr_server.py
+++ backend_server/asr_server.py
@@ -271,7 +271,7 @@ Lecture Transcript:
                 },
                 {"role": "user", "content": prompt}
             ],
-            temperature=0.7
+            temperature=0.7,
         )
 
         content = response.choices[0].message.content
